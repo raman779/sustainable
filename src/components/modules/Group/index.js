22a727d981bd39c9bd0b577c0b5e14c4610bd0d6
raman779 <a89791735@gmail.com>
2024-02-22 12:47:29 +0530

    new code

--- src/components/modules/Group/index.js
+++
@@ -1,8 +0,0 @@
-import PageFour from "./PageFour";
-import PageOne from "./PageOne";
-import PageThree from "./PageThree";
-import PageTwo from "./PageTwo";
-
-
-
-export {PageFour,PageThree,PageTwo,PageOne}
