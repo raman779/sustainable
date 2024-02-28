c48f88db126dad957a61aedb50f8bf4c189f1b1a
ravindra5438 <ravindrayadav5438@gmail.com>
2024-02-29 00:37:52 +0530

    gg

---
+++ frontend/src/store/UserProvider.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import UserContext from './context';
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (user) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
